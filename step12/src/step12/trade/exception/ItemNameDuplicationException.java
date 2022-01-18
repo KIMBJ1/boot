f362d5b477d97827ce1a10eca9ab794821f39abc
KIMBJ1 <qudwh9@naver.com>
2022-01-18 12:41:02 +0900

    [end] plz

--- step12/src/step12/trade/exception/ItemNameDuplicationException.java
+++ step12/src/step12/trade/exception/ItemNameDuplicationException.java
@@ -1,12 +1,11 @@
 package step12.trade.exception;
 
 public class ItemNameDuplicationException extends Exception {
-	
+
 	public ItemNameDuplicationException() {}
 
-	// 상속받은 message라는 변수에 값 대입하는 원리
 	public ItemNameDuplicationException(String msg) {
 		super(msg);
 	}
-		
+
 }
